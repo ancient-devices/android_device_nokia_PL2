54e35bfdefefeb49277245df6dc49b91a2e8008a
ANKIT KENE <keneankit01@gmail.com>
2022-02-01 13:37:06 +0530

    init ancient
    timepass

--- ancient_PL2.mk
+++ ancient_PL2.mk
@@ -19,13 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 #nherit from PL2 device
 $(call inherit-product, device/nokia/PL2/device.mk)
 
-#HYCON OS 
-HYCON_BUILD_TYPE := OFFICIAL
 
 # Inherit some common aosp stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -46,7 +44,7 @@ TARGET_OTA_ASSERT_DEVICE := PL2,PL2_sprout,Plate2
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := evolution_PL2
+PRODUCT_NAME := ancient_PL2
 PRODUCT_DEVICE := PL2
 PRODUCT_MANUFACTURER := HMD Global
 PRODUCT_BRAND := Nokia
